50e311c1a57b1e1cab1b14ff677abffa0ea3ce69
GeorgeRev <58150200+GeorgeRev@users.noreply.github.com>
2021-05-28 19:52:53 +0100

    Add files via upload

---
+++ MicroLed.ino
@@ -0,0 +1,32 @@
+#include <FastLED.h>
+
+#define LED_PIN 7
+#define READ_PIN A0
+
+#define NUM_LEDS      145
+#define BRIGHTNESS    255
+#define LED_TYPE      WS2812B
+#define COLOR_ORDER   GRB
+CRGB leds[NUM_LEDS];
+
+void setup() {
+
+  pinMode(READ_PIN,INPUT); //microphone
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+
+}
+
+void loop()
+{
+  
+  float soundstate = analogRead(READ_PIN);
+  int numLedsToLight = map(soundstate, 0, 1023, 0, NUM_LEDS);
+  FastLED.clear();
+  
+  for(int led = 0; led < numLedsToLight; led++) { 
+    leds[led] = CRGB::Red; 
+  }
+  FastLED.show();
+
+}
